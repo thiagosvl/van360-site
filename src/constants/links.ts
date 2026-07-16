3be0ffd2deb38422cbfa0d5d0f4241a708d75cb7
Thiago Barros <ext.thiago.abilio@mills.com.br>
2026-07-16 01:26:25 -0300

    fix: ajusta pesos de fontes da LP e adiciona mensagem customizada ao WhatsApp flutuante

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -4,7 +4,7 @@ const APP_BASE_URL = import.meta.env.PUBLIC_APP_URL || (IS_PROD ? "https://app.v
 export const LINKS = {
   PLAY_STORE_URL: "https://play.google.com/store/apps/details?id=com.tibis.van360",
   PLAY_STORE_BADGE_URL: "https://play.google.com/intl/en_us/badges/static/images/badges/pt-br_badge_web_generic.png",
-  WHATSAPP_SUPPORT_URL: "https://wa.me/5511962508068",
+  WHATSAPP_SUPPORT_URL: "https://wa.me/5511962508068?text=Oi,%20eu%20vim%20pelo%20site%20do%20Van360.",
   APP_LOGIN_URL: `${APP_BASE_URL}/login`,
   APP_REGISTER_URL: `${APP_BASE_URL}/cadastro`
 };
